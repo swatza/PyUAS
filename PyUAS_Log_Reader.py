a6da64522872a831db731d74c152f67daeacab80
spencer watza <s.g.watza@gmail.com>
2018-07-16 09:05:21 -0600

    Unknown change

--- PyUAS_Log_Reader.py
+++ PyUAS_Log_Reader.py
@@ -6,6 +6,6 @@ import PyPackets_pb2
 from google.protobuf import json_format
 
 
-name = "Mission_0.pypl"
+name = "Mission_2.pypl"
 myPacketLogger = PyPacketLogger.PyPacketLogger()
 myPacketLogger.openLogFile(name,True)
